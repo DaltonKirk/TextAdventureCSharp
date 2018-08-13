d09438385a4068c8729a64b013c02273a01f4ee9
Dalton <daltonkirk5@gmail.com>
2018-08-13 09:16:54 +0100

    Removed unused code from Item.cs

--- TextAdventure/Item.cs
+++ TextAdventure/Item.cs
@@ -8,40 +8,12 @@ namespace TextAdventure
 {    
 	class Item
 	{
-        
-        private List<Item> playerInventory;
         public string name = "";
         public bool canAdd = true;
         public bool isAccessible = false;
         public bool isEmpty = false;
         public bool isContainer = false;
         public int damage = 1;
-
-        public Item()
-		{
-            
-            playerInventory = new List<Item>();
-            
-		}
-
-        public void AddItem(Item item)
-        {
-            playerInventory.Add(item);
-
-        }
-        public void RemoveItem(Item item)
-        {
-
-        }
-
-        public List<Item> getInventory()
-        {
-            return new List<Item>(playerInventory);
-        }
-         
-        
-      
-
 
     }
 }
